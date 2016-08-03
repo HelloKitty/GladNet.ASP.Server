56dfff8d6e91537c6c78af0aba45f51444ce911a
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-03 17:18:04 -0500

    Added Formatter Extensions

---
+++ src/GladNet.ASP.Formatters/Extensions/GladNetFormatterExtensions.cs
@@ -0,0 +1,53 @@
+﻿using GladNet.ASP.Formatters;
+using GladNet.Serializer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//I know we shouldn't hijack Microsoft namespaces but it's so much easier for consumers
+//to access these extensions this way
+namespace Microsoft.AspNet.Builder
+{
+	/// <summary>
+	/// Extensions for Fluent MvcBuilder interfaces.
+	/// </summary>
+	public static class GladNetFormatterExtensions
+	{
+		/// <summary>
+		/// Adds the <see cref="ProtobufNetInputFormatter"/> and <see cref="ProtobufNetOutputFormatter"/> to the known formatters.
+		/// Also registers the Protobuf-net media header to map to these formatters.
+		/// </summary>
+		/// <param name="builder">Builder to chain off.</param>
+		/// <returns>The fluent <see cref="IMvcCoreBuilder"/> instance.</returns>
+		public static IMvcCoreBuilder AddProtobufNetFormatters(this IMvcCoreBuilder builder, ISerializerStrategy serializerStrat, IDeserializerStrategy deserializerStrat)
+		{
+			//Add the formatters to the options.
+			return builder.AddMvcOptions(options =>
+			{
+				options.InputFormatters.Add(new GladNetInputFormatter(deserializerStrat));
+				options.OutputFormatters.Add(new GladNetOutputFormatter(serializerStrat));
+				options.FormatterMappings.SetMediaTypeMappingForFormat("GladNet", MediaTypeHeaderValue.Parse("application/gladnet"));
+			});
+		}
+
+		/// <summary>
+		/// Adds the <see cref="ProtobufNetInputFormatter"/> and <see cref="ProtobufNetOutputFormatter"/> to the known formatters.
+		/// Also registers the Protobuf-net media header to map to these formatters.
+		/// </summary>
+		/// <param name="builder">Builder to chain off.</param>
+		/// <returns>The fluent <see cref="IMvcBuilder"/> instance.</returns>
+		public static IMvcBuilder AddProtobufNetFormatters(this IMvcBuilder builder, ISerializerStrategy serializerStrat, IDeserializerStrategy deserializerStrat)
+		{
+			//Add the formatters to the options.
+			return builder.AddMvcOptions(options =>
+			{
+				options.InputFormatters.Add(new GladNetInputFormatter(deserializerStrat));
+				options.OutputFormatters.Add(new GladNetOutputFormatter(serializerStrat));
+				options.FormatterMappings.SetMediaTypeMappingForFormat("GladNet", MediaTypeHeaderValue.Parse("application/gladnet"));
+			});
+		}
+	}
+}

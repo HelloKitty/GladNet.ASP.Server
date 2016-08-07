27e15482c7b02d550e92895a9243ea4e5ba8b960
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-07 16:13:22 -0500

    Update pre-MyGet.bat

--- pre-MyGet.bat
+++ pre-MyGet.bat
@@ -1,12 +1,12 @@
 cd ./src/GladNet.ASP.Server
 dotnet restore
-%nuget% update -noninteractive
+%nuget% update GladNet.ASP.Server.xproj -noninteractive
 cd ..
 cd ..
 
 cd ./src/GladNet.ASP.Formatters
 dotnet restore
-%nuget% update -noninteractive
+%nuget% update GladNet.ASP.Formatters.xproj -noninteractive
 cd ..
 cd ..
 

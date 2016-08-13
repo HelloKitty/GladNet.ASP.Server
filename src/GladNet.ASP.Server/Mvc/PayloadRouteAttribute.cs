31b8575d4169f5b76c8c1fa646f156ef3953b0b4
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-12 23:13:18 -0500

    Removed Generic Typestring on Payload and Payload Suffix

--- src/GladNet.ASP.Server/Mvc/PayloadRouteAttribute.cs
+++ src/GladNet.ASP.Server/Mvc/PayloadRouteAttribute.cs
@@ -14,11 +14,19 @@ namespace GladNet.ASP.Server
 	public class PayloadRouteAttribute : RouteAttribute
 	{
 		public PayloadRouteAttribute(Type payloadType) 
-			: base($"api/{payloadType.Name}")
+			: base($"api/{GetNameWithoutGenericArity(payloadType).TrimEnd("Payload".ToArray())}") //basically maps to api/PayloadName without generic stuff like '1 or the Payload identifier at the end of the TypeName.
 		{
 			//If it's not a child type then throw
 			if (!typeof(PacketPayload).IsAssignableFrom(payloadType))
 				throw new InvalidOperationException($"Provided Type is not a child of {nameof(PacketPayload)}. Type is: {payloadType?.FullName}.");
+		}
+
+		//based on: http://stackoverflow.com/questions/6386202/get-type-name-without-any-generics-info
+		private static string GetNameWithoutGenericArity(Type t)
+		{
+			string name = t.Name;
+			int index = name.IndexOf('`');
+			return index == -1 ? name : name.Substring(0, index);
 		}
 	}
 }
